d016adf77b85bf7831b2fac2b7f49917f85ff3ad
leticiadesouza <leticia.souza212@gmail.com>
2019-10-25 13:32:23 -0300

    Corrige exercicio 7

--- Preparatorios/exerciciosRepeticao/exercicio7.py
+++ Preparatorios/exerciciosRepeticao/exercicio7.py
@@ -1,11 +1,14 @@
 from random import randint
 
+// faca um algoritmo que funcione como um metodo de sorteio
+
 num = int(input('Digite um numero inteiro entre 1 e 10: '))
 soma = 0
 numero_sorteado = randint(1,10)
 print(numero_sorteado)
+
 while num != numero_sorteado:
     soma = soma + numero_sorteado
     numero_sorteado = randint(1,10)
     print(numero_sorteado)
-print('A soma eh', soma)
+print('A soma eh: ', soma)
